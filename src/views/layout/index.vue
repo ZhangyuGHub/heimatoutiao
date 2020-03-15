8901bb94f11c91afcb8c8ec707ffc9fdcd667163
ZhangyuGHub <1394956959@qq.com>
2020-03-15 21:06:02 +0800

    完成Layout组件及全局样式

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,18 @@
 <template>
-<div></div>
+<div class="container">
+    <van-nav-bar v-if="showNavBar" title="黑马头条" fixed right-text="搜索"></van-nav-bar>
+    <!-- 设置二级路由容器 -->
+    <div class="my-wrapper" :class="{noTop:!showNavBar}">
+        <router-view></router-view>
+    </div>
+    <!-- 设置底部标签并且开启路由模式 -->
+    <van-tabbar >
+  <van-tabbar-item to="/" icon="wap-home-o">首页</van-tabbar-item>
+  <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+  <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+  <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+</van-tabbar>
+</div>
 </template>
 
 <script>
