916477765f124f0b64c878949ade7e75ac67130b
ZhangyuGHub <1394956959@qq.com>
2020-03-15 21:51:49 +0800

    其他功能组件-骨架屏-基本编写

---
+++ src/views/search/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+    <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/video/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <div>
+      <van-skeleton title avatar :row="3"/>
+  </div>
+  <div>
+      <van-skeleton title avatar :row="3"/>
+  </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
