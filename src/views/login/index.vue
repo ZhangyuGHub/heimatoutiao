276a785f190d5a2c02989d7722fe74ae99ece1bc
ZhangyuGHub <1394956959@qq.com>
2020-03-18 17:01:05 +0800

    完成登录界面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class='container'>
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <van-cell-group>
+ <van-field
+    required
+    label="手机号"
+    placeholder="请输入手机号"
+  />
+  <van-field
+  center
+  required
+  clearable
+  label="验证码"
+  placeholder="请输入验证码"
+>
+  <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+</van-field>
+  </van-cell-group>
+   <div class='login-box'>
+      <van-button type="info" round size="small" block>登录</van-button>
+    </div>
   </div>
 </template>
 
@@ -11,5 +30,7 @@ export default {
 </script>
 
 <style>
-
+.login-box{
+  padding: 10px;
+}
 </style>
